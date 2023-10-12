fa210b4a2a2ccfb9a20751719848d037368caa80
morga <morganbakelmun@hotmail.com>
2023-10-11 22:04:26 -0400

    Make a simple beacon-seeker

--- python_code/simple_seeker.py
+++ python_code/simple_seeker.py
@@ -1,29 +1,25 @@
 def on_button_pressed_a():
     global counter
     counter += -1
-    basic.show_string("" + str((counter)))
+    basic.show_number(counter)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    if counter == 4:
-        basic.show_icon(IconNames.YES)
-    else:
-        basic.show_icon(IconNames.NO)
+    radio.send_number(counter)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+    basic.pause(200)
+    basic.clear_screen()
+radio.on_received_string(on_received_string)
 
 def on_button_pressed_b():
     global counter
     counter += 1
-    basic.show_string("" + str((counter)))
+    basic.show_number(counter)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 counter = 0
-index = 0
-while index < 0:
-    basic.show_string("2+2=?")
-    index += 1
+radio.set_group(1)
 counter = 0
-
-def on_forever():
-    pass
-basic.forever(on_forever)
