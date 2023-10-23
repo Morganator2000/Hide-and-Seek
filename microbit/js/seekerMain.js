1e857e34d76c00f273f37b5abcb7cef87f08b135
morga <morganbakelmun@hotmail.com>
2023-10-23 16:50:58 -0400

    Communication issues resolved.

--- microbit/js/seekerMain.js
+++ microbit/js/seekerMain.js
@@ -1,46 +1,35 @@
-//Startup sequence
-let signal = 0
-let answer = 0 //This is the position in the array when answering a question.
-let score = 0 //Players score. Will increase with a correct answer.
-let isSeeking = false //on startup, the seeker is not looking for beacons. This will quickly change.
-let seekerId = 0 //Assigned by application
-let options: string[] = [] //Your IDE is probably flagging an error here. Don't worry about it.
-options = ["N/A"] //The array for possible answers to a question. Currently nothing.
-radio.setGroup(1) //Set channel 1. All micro:bits will use this channel.
-basic.showString("S" + ("" + seekerId)) //Display seeker's id
-basic.pause(200)
-basic.clearScreen()
-isSeeking = true //Seeker is now looking for beacons.
 
-//This is the seeking function. Upon receiving a string from a beacon, LEDs light up.
-//Range is approximately 1 meter.
-radio.onReceivedString(function (receivedString) {
-    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    if (isSeeking == true && signal > -70) {
-        led.plotBarGraph(
-        Math.map(signal, -70, -42, 0, 9),
-        9
-        )
+// Code that runs when the seeker receives a number.
+radio.onReceivedNumber(function (receivedNumber) {
+    if (isSeeking == false) {
+        if (receivedNumber == 1) {
+            // answer is correct
+            basic.showIcon(IconNames.Yes)
+            basic.pause(1000)
+            basic.clearScreen()
+            score += 1
+            isSeeking = true
+        } else if (receivedNumber == 0) {
+            // answer is incorrect
+            basic.showIcon(IconNames.No)
+            basic.pause(1000)
+            basic.clearScreen()
+            isSeeking = true
+        } else {
+            basic.showString("Err")
+        }
     }
 })
-
-//These two listeners cycle through answers
+//Cycle through answers
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     answer = answer - 1
     if (answer < 0) {
         answer = options.length - 1
     }
     basic.showString("" + (options[answer]))
 })
-input.onButtonPressed(Button.B, function () {
-    answer = answer + 1
-    if (answer >= options.length) {
-        answer = 0
-    }
-    basic.showString("" + (options[answer]))
-})
-
-//Pressing both buttons either requests the question from the beacon or sends the answer.
+// Pressing both buttons either requests the question from the beacon or sends the answer.
 input.onButtonPressed(Button.AB, function () {
     if (options[0] == "N/A") {
         basic.clearScreen()
@@ -50,17 +39,58 @@ input.onButtonPressed(Button.AB, function () {
         options = ["N/A"]
     }
 })
-
-//Code for when the seeker receives a "key" (string with a value)
-//This covers a lot of things.
-radio.onReceivedValue(function (name, value) {
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -70) {
-        isSeeking = false //Turns off the signal while answering the question.
+// Seeker is now looking for beacons.
+// This is the seeking function. Upon receiving a string from a beacon, LEDs light up.
+// Range is approximately 1 meter.
+radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    if (isSeeking == true && signal > -70) {
+        led.plotBarGraph(
+        Math.map(signal, -70, -42, 0, 9),
+        9
+        )
+    }
+})
+//Cycle through answers
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    answer = answer + 1
+    if (answer >= options.length) {
         answer = 0
-        basic.showString("Q" + value) //Value here will be the beacon's ID. 
-        //The name is used to identify what type of question the user is answering
+    }
+    basic.showString("" + (options[answer]))
+})
+// Shaking the seeker will reveal the current score.
+input.onGesture(Gesture.Shake, function () {
+    if (isSeeking == true) {
+        // Breifly turns off the seeking function so the score is shown clearly.
+        isSeeking = false
+        basic.showNumber(score)
+        basic.pause(1000)
+        basic.clearScreen()
+        // Turn signal back on
+        isSeeking = true
+    } else {
+        // If the signal was never on, you don't need to turn it back on.
+        basic.showNumber(score)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+// Code for when the seeker receives a "key" (string with a value)
+// This covers a lot of things.
+radio.onReceivedValue(function (name, value) {
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -70 && name != "ask") {
+        // Turns off the signal while answering the question.
+        isSeeking = false
+        answer = 0
+        // Value here will be the beacon's ID.
+        basic.showString("Q" + value)
+        // The name is used to identify what type of question the user is answering
         if (name == "T/F") {
             options = ["T", "F"]
+        } else if (name == "M2") {
+            options = ["A", "B"]
         } else if (name == "M3") {
             options = ["A", "B", "C"]
         } else if (name == "M4") {
@@ -70,50 +100,33 @@ radio.onReceivedValue(function (name, value) {
             "C",
             "D"
             ]
-        } else if (name == "M5") {
-            options = [
-            "A",
-            "B",
-            "C",
-            "D",
-            "E"
-            ]
         } else {
             basic.showString("Err")
         }
         basic.showString("" + (options[0]))
     }
 })
-
-//Code that runs when the seeker receives a number.
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) { //answer is correct
-        basic.showIcon(IconNames.Yes)
-        basic.pause(1000)
-        basic.clearScreen()
-        score += 1
-        isSeeking = true
-    } else if (receivedNumber == 0) { //answer is incorrect
-        basic.showIcon(IconNames.No)
-        basic.pause(1000)
-        basic.clearScreen()
-        isSeeking = true
-    } else {
-        basic.showString("Err")
-    }
-})
-
-//Shaking the seeker will reveal the current score.
-input.onGesture(Gesture.Shake, function () {
-    if (isSeeking == true) {
-        isSeeking = false //Breifly turns off the seeking function so the score is shown clearly.
-        basic.showNumber(score)
-        basic.pause(1000)
-        basic.clearScreen()
-        isSeeking = true //Turn signal back on
-    } else { //If the signal was never on, you don't need to turn it back on.
-        basic.showNumber(score)
-        basic.pause(1000)
-        basic.clearScreen()
-    }
-})
+/**
+ * Startup sequence
+ */
+let signal = 0
+// This is the position in the array when answering a question.
+let answer = 0
+//Players score. Will increase with a correct answer.
+let score = 0
+// On startup, the seeker is not looking for beacons. This will quickly change.
+let isSeeking = false
+ //Assigned by application
+let seekerId = 0
+// Your IDE is probably flagging an error here. Don't worry about it.
+let options: string[] = []
+// The array for possible answers to a question. Currently nothing.
+options = ["N/A"]
+// Set channel 1. All micro:bits will use this channel.
+radio.setGroup(1)
+// Display seeker's id
+basic.showString("S" + ("" + seekerId))
+basic.pause(200)
+basic.clearScreen()
+// Seeker is now looking for beacons.
+isSeeking = true
