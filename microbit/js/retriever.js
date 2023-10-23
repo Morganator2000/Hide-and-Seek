09b5145dc1aea2a9d2d1be7ae58cc2a3006bcfdd
morga <morganbakelmun@hotmail.com>
2023-10-23 12:54:27 -0400

    Added documentation.

--- microbit/js/retriever.js
+++ microbit/js/retriever.js
@@ -1,3 +1,16 @@
+//Startup sequence
+let signal = 0
+radio.setGroup(1)
+radio.setTransmitPower(7) //broadcasts at strongest power.
+basic.showString("Searching", 50) //Startup displayed
+
+//Starts sending out this signal, calling the beacons
+basic.forever(function () {
+    radio.sendString("Homing")
+    basic.pause(101)
+})
+
+//When the retriever gets a reply, the graph displays a "hot-and-cold" led to guide the user to the beacon.
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Here") {
         signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
@@ -6,12 +19,4 @@ radio.onReceivedString(function (receivedString) {
         9
         )
     }
-})
-let signal = 0
-radio.setGroup(1)
-radio.setTransmitPower(7)
-basic.showString("Searching", 50)
-basic.forever(function () {
-    radio.sendString("Homing")
-    basic.pause(101)
 })
